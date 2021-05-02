002d5f319cd459f8923007ba34dc7bba39349ed7
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-05-02 18:25:27 +0900

    queue用意

--- save_serial.py
+++ save_serial.py
@@ -2,15 +2,31 @@ import sys
 import serial
 import datetime
 import csv
+from collections import deque
 
-ser = serial.Serial('/dev/cu.usbserial-1410', 115200) #ポートの情報を記入
+#ser = serial.Serial('/dev/cu.usbserial-14110', 115200) #ポートの情報を記入
+ser = serial.Serial('/dev/cu.usbserial-1430', 115200) #ポートの情報を記入
+
+d_red = deque()
+d_ir = deque()
 
 while(1):
     line = ser.readline().decode('utf-8')
-    splitted = line.split()
-    if len(splitted) > 3:
-        print(splitted)
-        with open('data.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(splitted)
+    splitted = line.split(",")
+    toCsv = list(map(int, splitted))
+    print(toCsv)
+    d_red.append(toCsv[0])
+    d_ir.append(toCsv[1])
+
+    if len(d_red) == 100:
+        r = d_red.popleft()
+        print("red", r)
+
+    if len(d_ir) == 100:
+        ir = d_ir.popleft()
+        print("ir", ir)
+
+    with open('data.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(toCsv)
     
